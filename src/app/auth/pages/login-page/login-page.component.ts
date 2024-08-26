bd6379f85709b2d974118b6a89502b6868368a11
Fernando Paz <ferpaz64@gmail.com>
2024-08-26 15:51:50 -0600

    Implementacion del servicio de autenticación para las paginas de Login y Dashboar para Logout

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from '../../services/auth-service';
+
+import Swal from 'sweetalert2';
+
 @Component({
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  private authService = inject( AuthService );
 
   private fb = inject( FormBuilder )
 
@@ -15,6 +21,16 @@ export class LoginPageComponent {
   });
 
   public login() {
-    console.log(this.myForm.value);
+    const { email, password} = this.myForm.value;
+
+    this.authService.login(email, password)
+      .subscribe({
+        //   No se necesita procesar el NEXT porque las rutas y los guards harán la redirección cuando
+        //   el login termina bien ya que actúan en base a los Signals
+        // next: () => {},
+        error: (errMessage) => {
+          Swal.fire(errMessage);
+        }
+      });
   }
 }
